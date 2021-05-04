53c7975b70fe3e959a76eb62aa9559e291b59939
Luncenok <idziejczak3@gmail.com>
2021-05-04 15:04:41 +0200

    Dodanie nowych typów struktur i podzielenie ich na 2 osobne enumy

---
+++ app/src/main/java/pl/edu/amu/home/popecol/bocianiegniazda/database/enums/Structure.kt
@@ -0,0 +1,10 @@
+package pl.edu.amu.home.popecol.bocianiegniazda.database.enums
+
+enum class Structure(val id: Int) {
+    POLE(1),
+    CHIMNEY(2),
+    ROOF(3),
+    TREE(4),
+    HUNTING_PULPIT(5),
+    HAYSTACK_STRAW_HAY(6)
+}
--- app/src/main/java/pl/edu/amu/home/popecol/bocianiegniazda/database/enums/StructureType.kt
+++ app/src/main/java/pl/edu/amu/home/popecol/bocianiegniazda/database/enums/StructureType.kt
@@ -1,10 +1,26 @@
 package pl.edu.amu.home.popecol.bocianiegniazda.database.enums
 
 enum class StructureType(val id: Int) {
-    PILLAR(1),
-    CHIMNEY(2),
-    ROOF(3),
-    TREE(4),
-    HUNTING_PULPIT(5),
-    HAYSTACK_STRAW_HAY(6)
+    OTHER(0),           //OTHER
+    FREE_STANDING(1),   //WOLNOSTOJĄCY
+    ELECTRIC(2),        //ELEKTRYCZY
+    WOODEN(3),          //DREWNIANY
+    CONCRETE(4),        //BETONOWY
+    RESIDENTIAL(5),     //MIESZKALNY
+    OUTBUILDING(6),     //GOSPODARCZY
+    TILE(7),            //DACHÓWKA
+    TIN(8),             //BLASZANY
+    THATCHED(9),        //ZE STRZECHY
+    PAPA(11),           //PAPA
+    ASBESTOS(12),       //ETERNIT
+    ALIVE(13),          //ŻYWE
+    DRY(14),            //SUCHE
+    BIRCH(15),          //BRZOZA
+    OAK(16),            //DĄB
+    ASH(17),            //JESION
+    MAPLE(18),          //KLON
+    LINDEN(19),         //LIPA
+    ACACIA_ROBINIA(20), //ROBINIA AKACJOWA
+    PINE(21),           //SOSNA
+    POLAR(22)           //POPLAR
 }
