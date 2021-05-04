374eb09ad82b620d76bfd39606c1676f61cad118
Luncenok <idziejczak3@gmail.com>
2021-05-04 14:29:55 +0200

    dodanie gniazd do bazy danych i typów struktur

---
+++ app/src/main/java/pl/edu/amu/home/popecol/bocianiegniazda/database/entities/Nest.kt
@@ -0,0 +1,41 @@
+package pl.edu.amu.home.popecol.bocianiegniazda.database.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users_table")
+data class Nest(
+    @PrimaryKey(autoGenerate = true)
+    val nestId: Long = 0L,
+
+    @ColumnInfo(name = "region")
+    val permissionType: String,
+
+    @ColumnInfo(name = "voivodeship")
+    val province: String,
+
+    @ColumnInfo(name = "district")
+    val district: String,
+
+    @ColumnInfo(name = "county")
+    val county: String,
+
+    @ColumnInfo(name = "town")
+    val town: String,
+
+    @ColumnInfo(name = "address")
+    val address: String,
+
+    @ColumnInfo(name = "longitude")
+    val longitude: String,
+
+    @ColumnInfo(name = "latitude")
+    val latitude: String,
+
+    @ColumnInfo(name = "is_nesting_platform")
+    val isNestingPlatform: Boolean,
+
+    @ColumnInfo(name = "structure_type")
+    val structureType: Int
+)
---
+++ app/src/main/java/pl/edu/amu/home/popecol/bocianiegniazda/database/enums/StructureType.kt
@@ -0,0 +1,10 @@
+package pl.edu.amu.home.popecol.bocianiegniazda.database.enums
+
+enum class StructureType(val id: Int) {
+    PILLAR(1),
+    CHIMNEY(2),
+    ROOF(3),
+    TREE(4),
+    HUNTING_PULPIT(5),
+    HAYSTACK_STRAW_HAY(6)
+}
